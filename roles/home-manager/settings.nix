7248ee1130102b834ce9112905b452ca9484b129
EricRoss <85009216+andersoic@users.noreply.github.com>
2023-07-02 17:51:05 -0600

    enable oh-my-zsh with apple theme

--- roles/home-manager/settings.nix
+++ roles/home-manager/settings.nix
@@ -6,6 +6,12 @@
   ];
   home-manager.useGlobalPkgs = true;
 
+  # Enable Oh-My-ZSH with Powerlevel 10k theme
+  programs.zsh.oh-my-zsh = {
+    enable = true;
+    theme = "apple";
+  };
+
   # Import nur as nixpkgs.overlays
   nixpkgs.overlays = [ 
     nur.overlay 
